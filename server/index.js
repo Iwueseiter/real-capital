34f461bb7ae014768c42b3cea4e83b29a0fcc9db
Iwueseiter <preciouschia19@gmail.com>
2026-04-16 10:33:04 +0100

    Setup Real Capital full stack backend, frontend, and database connection

--- server/index.js
+++ server/index.js
@@ -1,15 +1,21 @@
 const express = require("express");
-const app = express();
 const cors = require("cors");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+
+dotenv.config();
+connectDB();
+
+const app = express();
 
 app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("Real Capital API is running...");
+  res.send("Real Capital API Running");
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
